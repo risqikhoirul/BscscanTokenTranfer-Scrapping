8f4ad55add52d5f21abb4c34d4ccda0350b689d4
Risqi <khoirulrizki029@gmail.com>
2023-02-26 13:42:46 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# BscscanTokenTranfer-Scrapping
+
+## Install
+
+```
+git clone https://github.com/risqikhoirul/BscscanTokenTranfer-Scrapping.git
+cd BscscanTokenTranfer-Scrapping
+npm install
+```
+
+Or
+
+```
+npm i cheerio cheerio-tableparser
+```
+
+## Usage
+
+```
+node app <address>
+```
+
+Example:
+
+```
+node app 0x714cb1145218871faebd55de36dbe7053cc9c74d
+```
+
+## Output
+
+![](/output.png)
+
+## Licence
+
+This project is licensed under MIT License.
---
+++ app.js
@@ -0,0 +1,60 @@
+const cheerio = require("cheerio"),
+  cheerioTableparser = require("cheerio-tableparser");
+
+/**
+ * Author: M Khoirul Risqi
+ * Github: https://github.com/risqikhoirul/BscscanTokenTranfer-Scrapping
+ * Licence: MIT
+ * @param {node app <address>}
+ * example: node app 0x714cb1145218871faebd55de36dbe7053cc9c74d
+ * @returns:
+ * Object: {
+ *           success: boolean,
+ *           message: [{...},
+ *                     {...},
+ *                      ...]}
+ */
+
+const functionBscAddress = (address) =>
+  new Promise((resolve, reject) => {
+    try {
+      let message = [];
+      fetch(`https://bscscan.com/tokentxns?a=${address}&p=1`)
+        .then((res) => res.text())
+        .then((result) => {
+          const $ = cheerio.load(result);
+          cheerioTableparser($);
+          const resChe = $("table").parsetable(true, true, true);
+          const [hide, TxnHash, timestamp, age, from, outin, to, value, tokenName] = resChe;
+          for (let i = 1; i < hide.length; i++) {
+            message.push({
+              success: true,
+              TxnHash: TxnHash[i],
+              timestamp: timestamp[i],
+              age: age[i],
+              from: from[i],
+              outin: outin[i],
+              to: to[i],
+              value: value[i],
+              tokenName: tokenName[i],
+            });
+          }
+          resolve({
+            success: true,
+            message,
+          });
+        })
+        .catch((err) => reject(err));
+    } catch (err) {
+      resolve({
+        success: false,
+        message: err.message,
+      });
+    }
+  });
+
+(async () => {
+  const address = process.argv[2];
+  const readFunctionBscAddress = await functionBscAddress(address);
+  console.log(readFunctionBscAddress);
+})();
